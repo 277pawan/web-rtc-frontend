251fb155290e090bbb65e47c81d859146280e49e
perry.pawan <perry.pawan@rubicotech.in>
2025-05-29 16:21:19 +0530

    fix: fixed vite config.js file

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react"; // <-- ADD THIS
 
 export default defineConfig({
-  plugins: [react()], // <-- AND THIS
-
-  server: {
-    host: true,
-    allowedHosts: ["https://bvideo-caller.vercel.app/"],
-  },
+  plugins: [react()],
 });
