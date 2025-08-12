40dea06fb1c7e33a575977e1dbf87010dbea0ccb
277pawan <bpawan277@gmail.com>
2025-08-12 12:08:05 +0530

    feat: added Rotating.tsx component

--- src/components/heroVisuals/components/Card2.tsx
+++ src/components/heroVisuals/components/Card2.tsx
@@ -2,6 +2,7 @@ import Lottie from "lottie-react";
 import BirdsFlying from "../../../animation/red birds.json";
 import Dletter from "../../../animation/Alphabet D 3D rotation Lottie JSON animation.json";
 import SocialMedia from "../../../animation/Social media connection video with a mobile on hand.json";
+import RotatingText from "./RotatingText";
 
 const Card2 = () => {
   return (
@@ -16,7 +17,7 @@ const Card2 = () => {
         className="h-1/2 w-1/2 sm:max-h-screen sm:w-screen max-h-96 max-w-96"
       />
 
-      <div className="text-9xl font-extrabold flex flex-wrap items-center">
+      <div className="text-9xl font-extrabold flex flex-wrap items-center bg-gradient-to-t from-blue-600 to-green-300 bg-clip-text text-transparent ">
         <span>SeemLess&nbsp;Vi&nbsp;</span>
         <Lottie
           animationData={Dletter}
@@ -30,10 +31,24 @@ const Card2 = () => {
           }}
           className="-mx-16"
         />
-        <span>&nbsp;eo&nbsp;Calling</span>
+        <span>&nbsp;eo&nbsp;Calling:</span>
       </div>
 
-      <div className="text-9xl font-extrabold self-end">Get Connect</div>
+      <div className="flex text-9xl font-extrabold self-end bg-gradient-to-t from-blue-600 to-green-300 bg-clip-text text-transparent">
+        Get
+        <RotatingText
+          texts={["Connect", "Anytime", "Anywhere"]}
+          mainClassName="ml-4 px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+          staggerFrom={"last"}
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "-120%" }}
+          staggerDuration={0.025}
+          splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+          transition={{ type: "spring", damping: 30, stiffness: 400 }}
+          rotationInterval={2000}
+        />
+      </div>
       <div className="self-start ml-24">
         <Lottie
           animationData={SocialMedia}
